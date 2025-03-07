eb7e8266d4a655beea7dd01986f5dcf9dc73cb66
Alon Bochman <1891204+jswift24@users.noreply.github.com>
2025-03-07 17:59:12 -0500

    test experiment through api

---
+++ client/test_experiment.py
@@ -0,0 +1,51 @@
+#!pip install ragmetrics-client
+#!pip install openai litellm langchain_groq
+
+import ragmetrics
+from openai import OpenAI
+import litellm
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+# os.environ['RAGMETRICS_API_KEY'] = 'your_ragmetrics_key'
+# os.environ['GROQ_API_KEY'] = 'your_groq_key'
+# os.environ['OPENAI_API_KEY'] = 'your_openai_key'
+
+# Login with the API key from environment
+ragmetrics.login(base_url="http://localhost:8000")
+
+from ragmetrics import Cohort, Experiment, Task, Dataset, Example
+
+task1 = Task(
+    name="Test Task API",
+    generator_model="gpt-4o-mini",
+    system_prompt="Answer in English."
+)
+
+e1 = Example(
+    question="What is the biggest city in the US?",
+    ground_truth_context=["NYC is the biggest city in the US."],
+    ground_truth_answer="NYC"
+)
+e2 = Example(
+    question="Is it beautiful?",
+    ground_truth_context=["NYC is known for its beauty."],
+    ground_truth_answer="Yes"
+)
+dataset1 = Dataset(examples = [e1, e2], name="API Dataset")
+
+cohort1 = Cohort(name="gpt-4o-mini", generator_model="gpt-4o-mini")
+cohort2 = Cohort(name="API Demo: Stub", rag_pipeline="API Demo: Stub")
+
+exp_models = Experiment(
+    name="Model Experiment",
+    dataset=dataset1,   
+    task=task1,         
+    cohorts=[cohort1, cohort2],
+    criteria=["Accuracy", "QA_Context mentions a phone"],
+    judge_model="gpt-4o-mini"
+)
+
+final_progress_data = exp_models.run()
